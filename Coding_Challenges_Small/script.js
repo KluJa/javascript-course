fc17ddef92026401f24f45ed631cf0ff5ff10af5
Jan Klust <jan.klust@gmail.com>
2021-01-10 20:52:02 +0100

    Initial Commit

--- Coding_Challenges_Small/script.js
+++ Coding_Challenges_Small/script.js
@@ -1,21 +1,23 @@
-'use strict'
+"use strict";
 
 //Assortment of small challenges that did not have their own chapter
 
-// #1 Getting values out of an object 
+// #1 Getting values out of an object
 
 const jonas = {
-    firstName : 'Jonas',
-    lastName : 'Schmedtmann',
-    friends : ['Michael' ,'Peter' ,'Steven'],
-    age: 46,
-    job: 'teacher',
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  friends: ["Michael", "Peter", "Steven"],
+  age: 46,
+  job: "teacher",
 
-    jonasSummary : function () {
-         return `${this.firstName} is a ${this.age} old ${this.job}, who has ${this.friends.length} friends whose names are: ${this.friends}`;
-    }
+  jonasSummary: function () {
+    return `${this.firstName} is a ${this.age} old ${this.job}, who has ${this.friends.length} friends whose names are: ${this.friends}`;
+  },
 };
 
-console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[1]}`);
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[1]}`
+);
 
 console.log(jonas.jonasSummary());
