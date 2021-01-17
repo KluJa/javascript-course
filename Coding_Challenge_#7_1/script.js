f85f17454ef59bd1bab33d9f488e539bc1a06305
Jan Klust <ynhojk@gmail.com>
2021-01-17 19:57:34 +0100

    Refactored code of #7_1. Created separate fuctions for repetitive tasks

--- Coding_Challenge_#7_1/script.js
+++ Coding_Challenge_#7_1/script.js
@@ -1,52 +1,69 @@
 "use strict";
 
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
+// Function meant for replacing query selector code,
+// 1st attribute is the given HTML node or its .class, needs to be in ""
+// 2nd attribute is the value/or message we want the given attribute to display, strings need "" or ``
+function changeText(node, text) {
+	document.querySelector(node).textContent = text;
+}
 
-console.log(randomNumber);
-
-function guessing() {
-  const guess = Number(document.querySelector(".guess").value);
-
-  if (guess < 1 && guess > 20) {
-    // Illegal Value on the input
-    document.querySelector(".number").textContent = guess;
-    document.querySelector(".message").textContent =
-      "Invalid value, please select number between 1 and 20";
-  } else if (guess === randomNumber) {
-    // Correct Guess
-    document.querySelector(".number").textContent = randomNumber; //
-    document.querySelector(".message").textContent = "You are correct";
-    document.querySelector("body").style.backgroundColor = "green";
-    document.querySelector(".number").style.width = "30rem";
-    score++;
-    document.querySelector(".score").textContent = score;
-  } else if (guess < randomNumber) {
-    // Low Guess
-    document.querySelector(".message").textContent =
-      "You are not correct. Number too Low";
-    score--;
-    document.querySelector(".score").textContent = score;
-  } else if (guess > randomNumber) {
-    // High Guess
-    document.querySelector(".message").textContent =
-      "You are not correct. Number too high";
-    score--;
-    document.querySelector(".score").textContent = score;
-  }
+// Function to replace the repetitve code for too low and too high value guess.
+// input is a string that tells whether the value is low or high relative to generated Number.
+function lowHighGuess(valueSize) {
+	score--;
+	changeText(".message", `You are not correct. Number too "${valueSize}"`);
+	changeText(".score", score);
 }
 
 // Function for reseting the game. Sets background to black
 function reset() {
-  const guess = Number(document.querySelector(".guess").value);
-  score = 20;
-  document.querySelector("body").style.backgroundColor = "#222";
-  document.querySelector(".number").style.width = "15rem";
-  document.querySelector(".number").textContent = "?";
-  document.querySelector(".guess").value = "";
-  document.querySelector(".message").textContent = "Start guessing...";
-  document.querySelector(".score").textContent = score;
+	const guess = Number(document.querySelector(".guess").value);
+	score = 20;
+	document.querySelector("body").style.backgroundColor = "#222";
+	document.querySelector(".number").style.width = "15rem";
+	changeText(".number", "?");
+	document.querySelector(".guess").value = "";
+	changeText(".message", "?");
+	changeText(".score", score);
+
+	// document.querySelector(".number").textContent = "?";
+	// document.querySelector(".message").textContent = "Start guessing...";
+	// document.querySelector(".score").textContent = score;
 }
+
+//Function faciliating the main logic of the "game"
+function guessing() {
+	const guess = Number(document.querySelector(".guess").value);
+	// ----- Invalid input
+	if (guess < 1 && guess > 20) {
+		changeText(".number", guess);
+		changeText(
+			".message",
+			"Invalid value, please select number between 1 and 20"
+		);
+	} // ----- Correct Guess
+	else if (guess === randomNumber) {
+		changeText(".number", randomNumber);
+		document.querySelector("body").style.backgroundColor = "green";
+		document.querySelector(".number").style.width = "30rem";
+		score++;
+		changeText(".score", score);
+		changeText(".message", "You are Correct!");
+		//Validate that score is higher than highscore and, if true, set highscore
+		score > highscore ? (highscore = score) : highscore;
+	} // ----- Low Guess
+	else if (guess < randomNumber) {
+		lowHighGuess(low);
+	} // ----- High Guess
+	else if (guess > randomNumber) {
+		lowHighGuess(high);
+	}
+}
+
+// ----- Main Processing -----
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", guessing);
 document.querySelector(".again").addEventListener("click", reset);
