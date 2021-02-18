e2559c8304e8645e661181004fc09885571dc336
Jan Klust <jan.klust@gmail.com>
2021-02-18 08:48:07 +0100

    Updated challenge 9.1

--- Coding_Challenge_#9_1/script.js
+++ Coding_Challenge_#9_1/script.js
@@ -1,1 +1,61 @@
-console.log("hello world");
+const game = {
+  team_1: "Bayern_Munich",
+  team_2: "Borrussia_Dortmund",
+
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies ",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weiml",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+
+  score: "4.0",
+  scored: ["Lewandovski", "Gnarby", "Lewandovski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players_1, players_2] = game.players;
+console.log(players_1, players_2);
+
+const [gk_bayern, ...fieldPlayers_bayern] = players_1;
+
+console.log(gk_bayern);
+console.log(fieldPlayers_bayern);
+
+const allPlayers = [...players_1, ...players_2];
+console.log(allPlayers);
+
+const players1Final = [...players_1, "Thiago", "Coutinho", "Perisic"];
+console.log(players1Final);
+
+const { team1, x, team2 } = game.odds;
+console.log(team1);
+console.log(x);
+console.log(team2);
